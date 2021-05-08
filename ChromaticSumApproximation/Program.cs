1f59322a4d3957440cd41aceffca8b63230bc916
Pawel Pohl <pawel.pohl.stud@pw.edu.pl>
2021-05-08 22:35:00 +0200

    Add project files.

---
+++ ChromaticSumApproximation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChromaticSumApproximation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
