ba28656c646292bfb9038e99330e8e9f6ccaec1c
Pawel Pohl <pawel.pohl.stud@pw.edu.pl>
2021-05-09 15:43:39 +0200

    GraphFactory added method for parsing list of graphs.

--- ChromaticSumApproximation/Factories/GraphFactory.cs
+++ ChromaticSumApproximation/Factories/GraphFactory.cs
@@ -3,8 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChromaticSumApproximation.Factories
 {
@@ -13,11 +11,27 @@ namespace ChromaticSumApproximation.Factories
         public Graph GraphFromAdjacencyList(string filename)
         {
             var lines = File.ReadAllLines(filename).Where(l => !string.IsNullOrEmpty(l.Trim())).ToArray();
+            return GetGraphFromList(lines);
+        }
+
+        public List<Graph> GraphsFromAdjacencyLists(string filename)
+        {
+            var text = File.ReadAllText(filename);
+            var lists = text.Split("\n\n");
+            var graphs = new List<Graph>();
+
+            foreach (var list in lists)
+                graphs.Add(GetGraphFromList(list.Split('\n')));
+
+            return graphs;
+        }
+
+        private Graph GetGraphFromList(string[] lines)
+        {
             if (lines.Length == 0)
                 throw new ArgumentException("Adjacency List was empty");
 
             var graph = new Graph();
-            
             for (int i = 0; i < lines.Length; ++i)
                 graph.Vertices.Add(new Vertex(i));
 
@@ -35,5 +49,7 @@ namespace ChromaticSumApproximation.Factories
 
             return graph;
         }
+
+
     }
 }
--- ChromaticSumApproximation/Program.cs
+++ ChromaticSumApproximation/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ChromaticSumApproximation.Algorithms;
 using ChromaticSumApproximation.Factories;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ChromaticSumApproximation
@@ -10,17 +11,46 @@ namespace ChromaticSumApproximation
         static void Main(string[] args)
         {
             var graphFactory = new GraphFactory();
-            var graph = graphFactory.GraphFromAdjacencyList("../../../Graphs/graph_2505.lst");
+            //var graph = graphFactory.GraphFromAdjacencyList("../../../Graphs/graph_41669.lst");
+            var graphs = graphFactory.GraphsFromAdjacencyLists("../../../Graphs/list_170_graphs.lst");
 
-            foreach (var vertex in graph.Vertices)
-                Console.WriteLine($"{vertex.Index + 1}: {string.Join(' ', vertex.Neighbors.Select(n => (n.Index + 1).ToString()))}");
+            //foreach (var vertex in graph.Vertices)
+            //Console.WriteLine($"{vertex.Index + 1}: {string.Join(' ', vertex.Neighbors.Select(n => (n.Index + 1).ToString()))}");
+            var smallestFirst = new SmallestFirst();
+            var dSatur = new ModifiedDSatur();
+            var sums = new List<Tuple<int, int>>();
 
-            var algo = new ModifiedDSatur();
-            var sum = algo.ApproximateChromaticSum(graph);
+            foreach (var graph in graphs)
+            {
+                var sumSF = smallestFirst.ApproximateChromaticSum(graph);
+                graph.ResetColors();
+                var sumDS = dSatur.ApproximateChromaticSum(graph);
 
-            Console.WriteLine($"Chromatic sum: {sum}");
-            foreach (var vertex in graph.Vertices)
-                Console.WriteLine($"{vertex.Index + 1} : {vertex.Color.Value}");
+                sums.Add(new Tuple<int, int>(sumSF, sumDS));
+
+                //Console.WriteLine($"SmallestFirst chromatic sum : {sumSF}");
+                //Console.WriteLine($"ModifiedDSatur chromatic sum : {sumDS}");
+                //Console.WriteLine();
+            }
+            Console.WriteLine(sums.Where(t => t.Item1 < t.Item2).Count());
+            Console.WriteLine(sums.Where(t => t.Item1 <= t.Item2).Count());
+            
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    graph.ResetColors();
+
+            //    var algo = new SmallestFirst();
+            //    var sum = algo.ApproximateChromaticSum(graph);
+
+            //    Console.WriteLine($"Chromatic sum: {sum}");
+
+            //    graph.ResetColors();
+
+            //    var algo2 = new ModifiedDSatur();
+            //    var sum2 = algo2.ApproximateChromaticSum(graph);
+
+            //    Console.WriteLine($"Chromatic sum: {sum2}");
+            //}
         }
     }
 }
