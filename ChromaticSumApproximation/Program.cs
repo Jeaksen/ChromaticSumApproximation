6cf8a2864d6106a66be91e9ef3fbc2bbb9987808
Pawel Pohl <pawel.pohl.stud@pw.edu.pl>
2021-05-14 16:46:46 +0200

    Directory methods usage added to main function for file loading to work on linux and windows.

--- ChromaticSumApproximation/Program.cs
+++ ChromaticSumApproximation/Program.cs
@@ -3,23 +3,50 @@ using ChromaticSumApproximation.Factories;
 using ChromaticSumApproximation.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace ChromaticSumApproximation
 {
     class Program
     {
+        private static DirectoryInfo singleGraphs;
+        private static DirectoryInfo listsGraphs;
+
+        private static string GetSingleGraphPath(string name)
+        {
+            var file = singleGraphs.GetFiles(name).FirstOrDefault();
+            if (file == default || !file.Exists)
+                throw new ArgumentException($"Failed to find {name} in Graphs/Single directory");
+            return file.FullName;
+        }
+
+        private static string GetGraphListsPath(string name)
+        {
+            var file = listsGraphs.GetFiles(name).FirstOrDefault();
+            if (file == default || !file.Exists)
+                throw new ArgumentException($"Failed to find {name} in Graphs/Lists directory");
+            return file.FullName;
+        }
+
         static void Main(string[] args)
         {
+            string cwd = Directory.GetCurrentDirectory();
+            DirectoryInfo graphDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(cwd).FullName).FullName).GetDirectories("Graphs").FirstOrDefault();
+            singleGraphs = graphDir.GetDirectories("Single").FirstOrDefault();
+            listsGraphs = graphDir.GetDirectories("Lists").FirstOrDefault();
+            if (singleGraphs == default || listsGraphs == default)
+                throw new Exception("Failed to load the Single and Lists graphs directories!");
+
             var graphFactory = new GraphFactory();
             var graphs = new List<Graph>();
-
+            
             // one graph with 208 vertices
-            //graphs.Add(graphFactory.GraphFromAdjacencyList("../../../Graphs/Single/graph_41669.lst"));
+            //graphs.Add(graphFactory.GraphFromAdjacencyList(GetSingleGraphPath("graph_41669.lst")));
             // 170 big graphs, over 240 vertices
-            //graphs.AddRange(graphFactory.GraphsFromAdjacencyLists("../../../Graphs/Lists/list_170_graphs.lst"));
+            //graphs.AddRange(graphFactory.GraphsFromAdjacencyLists(GetGraphListsPath("list_170_graphs.lst")));
             // 22 graphs with  different sizes and bounds calculation
-            graphs.AddRange(graphFactory.GraphsFromAdjacencyLists("../../../Graphs/Lists/chidx_22_graphs.lst"));
+            graphs.AddRange(graphFactory.GraphsFromAdjacencyLists(GetGraphListsPath("chidx_22_graphs.lst")));
 
             var smallestFirst = new SmallestFirst();
             var dSatur = new ModifiedDSatur();
